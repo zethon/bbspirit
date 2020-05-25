20b4f22874625bb45a0f6d6c6e144157dcd24019
Addy <aclaure@gmail.com>
2020-05-25 13:58:40 -0400

    rearranging things;

---
+++ tests/test_misc.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+
+using namespace std::string_literals;
+
+BOOST_AUTO_TEST_SUITE(misc)
+
+BOOST_AUTO_TEST_CASE(CommandHistory)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END() // arccutils
