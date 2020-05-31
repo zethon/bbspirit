8e42803b1419edfe1c156a6da1be52f682b330d1
Addy <aclaure@gmail.com>
2020-05-31 09:09:32 -0400

    compiles

--- include/bbspirit.hpp
+++ include/bbspirit.hpp
@@ -6,36 +6,109 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_pair.hpp>
 
+namespace x3 = boost::spirit::x3;
+
 namespace bbspirit
 {
 
-namespace x3 = boost::spirit::x3;
-
-struct node;
-
-using node_value_base = x3::variant <
-    std::string,
-    x3::forward_ast<node>
->;
-    
-struct node_value : node_value_base
+struct SimpleElement
 {
-    using base_type::base_type;
-    using base_type::operator=;
-};
+    int age;
+    std::string forename;
+    std::string surname;
+    double salary;
 
-using node_vector = std::vector<node_value>;
+//    std::string name;
+//    std::string content;
 
-struct node
-{
-    node_vector children;
+//    friend std::ostream& operator<<(std::ostream& os, const SimpleElement& el)
+//    {
+//        os << '[' << el.name << ']' << el.content << "[/" << el.name << ']';
+//        return os;
+//    }
 };
 
 } // namespace bbspirit
 
-BOOST_FUSION_ADAPT_STRUCT
-(
-    bbspirit::node, children
+//BOOST_FUSION_ADAPT_STRUCT
+//(
+//    bbspirit::SimpleElement, name, content
+//)
+
+BOOST_FUSION_ADAPT_STRUCT(bbspirit::SimpleElement,
+    age, forename, surname, salary
 )
+
+namespace bbspirit
+{
+
+    namespace x3 = boost::spirit::x3;
+    namespace ascii = boost::spirit::x3::ascii;
+
+
+    using x3::int_;
+    using x3::lit;
+    using x3::double_;
+    using x3::lexeme;
+    using ascii::char_;
+
+    const x3::rule<class SimpleElementID, SimpleElement> employee = "employee";
+
+    auto const quoted_string = lexeme['"' >> +(char_ - '"') >> '"'];
+
+    auto const employee_def =
+        lit("employee")
+        >> '{'
+        >>  int_ >> ','
+        >>  quoted_string >> ','
+        >>  quoted_string >> ','
+        >>  double_
+        >>  '}'
+        ;
+
+    BOOST_SPIRIT_DEFINE(employee);
+
+//struct SimpleElementID{};
+//const x3::rule<struct SimpleElementID, SimpleElement> simpleTag = "simpleTag";
+
+//const auto simpleTag_def =
+//        x3::lexeme['[' >> +(x3::char_ - ']') >> ']' >> +(x3::char_)];
+
+//BOOST_SPIRIT_DEFINE(simpleTag);
+
+} // namespace bbspirit
+
+
+//namespace bbspirit
+//{
+
+//namespace x3 = boost::spirit::x3;
+
+//struct node;
+
+//using node_value_base = x3::variant <
+//    std::string,
+//    x3::forward_ast<node>
+//>;
+    
+//struct node_value : node_value_base
+//{
+//    using base_type::base_type;
+//    using base_type::operator=;
+//};
+
+//using node_vector = std::vector<node_value>;
+
+//struct node
+//{
+//    node_vector children;
+//};
+
+//} // namespace bbspirit
+
+//BOOST_FUSION_ADAPT_STRUCT
+//(
+//    bbspirit::node, children
+//)
 
 
--- tests/test_misc.cpp
+++ tests/test_misc.cpp
@@ -8,6 +8,16 @@ BOOST_AUTO_TEST_SUITE(misc)
 
 BOOST_AUTO_TEST_CASE(CommandHistory)
 {
+    std::string test = "[b]Hello World[/b]"s;
+
+    std::string::const_iterator start = test.begin();
+    const std::string::const_iterator stop = test.end();
+
+    bbspirit::SimpleElement element{};
+
+    bool result =
+        phrase_parse(start, stop, bbspirit::employee, x3::ascii::space, element);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END() // arccutils
