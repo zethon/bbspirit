6370eb5e32d21807c7d23fda3454d78e433d167c
Addy <aclaure@gmail.com>
2020-06-01 15:42:41 -0400

    top

--- include/bbspirit.hpp
+++ include/bbspirit.hpp
@@ -6,6 +6,12 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_pair.hpp>
 
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/object.hpp>
+#include <boost/phoenix/bind.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+
 namespace x3 = boost::spirit::x3;
 
 namespace bbspirit
@@ -53,19 +59,57 @@ namespace bbspirit
     using x3::double_;
     using x3::lexeme;
     using ascii::char_;
+    using ascii::string;
 
     const x3::rule<class SimpleElementID, SimpleElement> employee = "employee";
 
     auto const quoted_string = lexeme['"' >> +(char_ - '"') >> '"'];
 
-    auto const employee_def =
-        lit("employee")
-        >> '{'
-//        >>  int_ >> ','
-        >>  quoted_string >> ','
-        >>  quoted_string >> ','
-//        >>  double_
-        >>  '}'
+    auto db = [](auto& ctx)
+    {
+        std::cout << "db: " << x3::_attr(ctx) << '\n';
+    };
+
+//    auto db2 = [](auto& ctx)
+//    {
+//        std::cout << "db2: " << x3::_attr(ctx) << '\n';
+//    };
+
+    auto assignTag = [](auto& ctx)
+    {
+        std::cout << "->: " << x3::_attr(ctx) << '\n';
+        x3::_val(ctx).tag = x3::_attr(ctx);
+    };
+
+    auto testTag = [](auto& ctx)
+    {
+        auto& element = x3::_val(ctx);
+////        element.tag = x3::_attr(ctx);
+//        element.content = x3::_attr(ctx);
+
+        std::cout << "a: " << element.tag << '\n';
+        std::cout << "b: " << x3::_attr(ctx) << '\n';
+        std::cout << "hi";
+
+//        x3::_pass(ctx) = (element.tag == x3::_attr(ctx));
+
+//        if (x3::_val(ctx).tag = x3::_attr(ctx))
+//        {
+//            std::cout << "hi"
+//        }
+//        std::string temp = x3::_attr(ctx);
+//        x3::_pass(ctx) = x3::_attr(ctx)
+//                == x3::_val(ctx).tag;
+    };
+
+    auto const employee_def
+        = '['
+        >> +(char_ - ']')[assignTag]
+        >> ']'
+        >> +(char_ - x3::lit("[/"))[db]
+        >> x3::lit("[/")
+        >> +(char_ - ']')[testTag]
+        >> ']'
         ;
 
     BOOST_SPIRIT_DEFINE(employee);
--- tests/test_misc.cpp
+++ tests/test_misc.cpp
@@ -1,14 +1,17 @@
+#include <iostream>
+
 #include <boost/test/unit_test.hpp>
 
 #include "../include/bbspirit.hpp"
 
 using namespace std::string_literals;
 
-BOOST_AUTO_TEST_SUITE(misc)
+BOOST_AUTO_TEST_SUITE(bbspirit)
 
-BOOST_AUTO_TEST_CASE(CommandHistory)
+// --run_test=bbspirit/simpleTag
+BOOST_AUTO_TEST_CASE(simpleTag)
 {
-    std::string test = "[b]Hello World[/b]"s;
+    std::string test = "[be]Hello World[/be]"s;
 
     std::string::const_iterator start = test.begin();
     const std::string::const_iterator stop = test.end();
@@ -18,6 +21,10 @@ BOOST_AUTO_TEST_CASE(CommandHistory)
     bool result =
         phrase_parse(start, stop, bbspirit::employee, x3::ascii::space, element);
 
+    BOOST_REQUIRE(result);
+    BOOST_REQUIRE(start == stop);
+    BOOST_REQUIRE_EQUAL(element.tag, "b");
+    BOOST_REQUIRE_EQUAL(element.content, "Hello World");
 }
 
 BOOST_AUTO_TEST_SUITE_END() // arccutils
